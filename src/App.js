59fa9ad2369a6d728af0fb356fbb69ac92a76a0b
MagdK <kovacsmagda@gmail.com>
2022-07-03 18:27:46 +0200

    Set up shop link and logo link by importing Link from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -21,7 +12,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index={true} element={<Home />} />
+        <Route index={true} element={<Home />} /> 
+        {/* index without ={true} works as well*/}
         <Route path="shop" element={<Shop /> } />
       </Route>
     </Routes>
