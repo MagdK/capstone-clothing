573d409a00e3831e0a3dc963d0c6f1bdfdd8fa5c
MagdK <kovacsmagda@gmail.com>
2022-07-04 11:25:17 +0200

    Create assets folder, upload site logo, import logo to navigation components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 
-
 const Shop = () => {
   return <h1>I am the shop page</h1>;
 }
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
 
+import { ReactComponent as SiteLogo } from '../../assets/site_logo.svg';
+
 const Navigation = () => {
     return (
       <Fragment>
         <div className="navigation">
             <Link className="logo-container" to="/">
-                <div>Logo</div>
+                <SiteLogo />
             </Link>
           <div className="nav-links-container">
               <Link className="nav-link" to="/shop">
