df27afb38d785ecc3d1b139e3ad16119d0374c9a
MagdK <kovacsmagda@gmail.com>
2022-07-04 11:34:56 +0200

    Add css reset to index.scss, style navigation with flex

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,8 @@ import { Fragment } from 'react';
 
 import { ReactComponent as SiteLogo } from '../../assets/site_logo.svg';
 
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
       <Fragment>
