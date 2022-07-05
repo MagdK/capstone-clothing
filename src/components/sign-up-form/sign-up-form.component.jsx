49f52a21fce2dcb6722410f93a3744a21740d8dc
MagdK <kovacsmagda@gmail.com>
2022-07-05 15:36:48 +0200

    Sign up submit: check if password and confirm password match, on form submit, call the handleSubmit function

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -17,7 +17,17 @@ const SignUpForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        
+        if(password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+
+        try {
+            const response = await createAuthUserWithEmailAndPassword(email, password);
+            console.log(response);
+        } catch(error) {
+            console.log('user creation encountered an error', error);
+        }
     }
 
     const handleChange = (event) => {
@@ -29,7 +39,7 @@ const SignUpForm = () => {
     return (
         <div>
             <h1>Sign up with your email and password</h1>
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit}>
                 <label>Display name</label>
                 <input 
                     type="text" 
