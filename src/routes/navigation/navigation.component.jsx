78b0a83045248245ac61020b945c8383a48036dd
MagdK <kovacsmagda@gmail.com>
2022-07-06 12:12:16 +0200

    Set up sign out flow with context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,25 +4,37 @@ import { Fragment, useContext } from 'react';
 import { ReactComponent as SiteLogo } from '../../assets/site_logo.svg';
 import { UserContext } from '../../contexts/user.context';
 
+import {signOutUser} from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
     console.log(currentUser);
-    
+
+    const signOutHandler = async () => {
+        await signOutUser();
+        setCurrentUser(null);
+    }
+
     return (
       <Fragment>
         <div className="navigation">
             <Link className="logo-container" to="/">
                 <SiteLogo />
             </Link>
-          <div className="nav-links-container">
-              <Link className="nav-link" to="/shop">
+        <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
                 Shop
-              </Link>
-              <Link className="nav-link" to="/auth">
-                Sign in
-              </Link>
+            </Link>
+            {currentUser ? (
+                <span className="nav-link" onClick={signOutHandler}>
+                    Sign out
+                </span>
+            ) : ( 
+                <Link className="nav-link" to="/auth">
+                    Sign in
+                </Link>
+            )}
           </div>
         </div>
         <Outlet />
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,7 +6,8 @@ import {
     signInWithPopup, 
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut
 } from 'firebase/auth';
 
 import {
@@ -89,3 +90,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
     return signInWithEmailAndPassword(auth, email, password);
 }
+
+export const signOutUser = async () => await signOut(auth);
